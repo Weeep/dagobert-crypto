b1b0d8e8110544f65231653f4657257b207f8c9c
Dániel Nováki <szept26@gmail.com>
2024-01-25 22:18:51 +0100

    update

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,31 +2,62 @@
 
 import React, { useState, useEffect } from 'react';
 
+/*
+"symbol": "SOLUSDT",
+"orderId": 2805178958,
+"executedQty": "0.62000000",
+"cummulativeQuoteQty": "24.90540000",
+"status": "FILLED",
+"type": "LIMIT",
+"side": "BUY",
+"updateTime": 1654168978315,
+*/
+interface Transaction {
+    symbol: string;
+    orderId: number;
+    executedQty: string;
+    cummulativeQuoteQty: string;
+    status: string;
+    type: string;
+    side: string;
+    updateTime: number;
+}
+
+function formatTransaction(transaction: Transaction) {
+    return (
+        <>
+            <hr />
+            <p>{transaction.orderId}</p>
+            <p>{transaction.updateTime}</p>
+            <p>{transaction.side}</p>
+            <hr />
+        </>
+    );
+}
+
 const TestPage: React.FC = () => {
     console.log('test')
-  const [apiResponse, setApiResponse] = useState<string | null>(null);
+  const [apiResponse, setApiResponse] = useState<Transaction[]>([]);
 
   useEffect(() => {
-    setApiResponse("5");
-    /*
     const fetchData = async () => {
       try {
         const response = await fetch('/api/hello');
         const data = await response.json();
         console.log(data)
-        setApiResponse(data.message);
+        setApiResponse(data);
       } catch (error: any) {
         console.error('Error fetching data:', error.message);
       }
     };
 
-    fetchData();*/
+    fetchData();
   }, []); // Empty dependency array ensures the effect runs only once, similar to componentDidMount
 
   return (
     <div>
       <h1>API Response:</h1>
-      <p>{apiResponse}</p>
+      {apiResponse.map(transaction => formatTransaction(transaction))}
     </div>
   );
 };
