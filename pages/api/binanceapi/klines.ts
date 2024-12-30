031deb9fe377aef8f9ce618adecdc0084f1ad629
Dániel Nováki <szept26@gmail.com>
2024-12-30 10:01:57 +0100

    klines.ts update

--- pages/api/binanceapi/klines.ts
+++ pages/api/binanceapi/klines.ts
@@ -1,15 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import binanceapiutils from "../../../utils/binanceapiutil";
+import { ApiResponse } from "@/utils/types";
 
 export default async function klines(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { resultCode, resultBody } = await libKlines(req.query);
-  res.status(resultCode).json(resultBody);
+  const apiResponse: ApiResponse = await libKlines(req.query);
+  if (apiResponse.ok) {
+    res.status(apiResponse.code).json(JSON.stringify(apiResponse.response));
+  } else {
+    res.status(apiResponse.code).json({ error: apiResponse.error });
+  }
 }
 
-export async function libKlines({ symbol = "", interval = "" }) {
+export async function libKlines({
+  symbol = "",
+  interval = "",
+}): Promise<ApiResponse> {
   let resultCode: number = 500;
   let resultBody: string = "";
 
@@ -21,9 +29,12 @@ export async function libKlines({ symbol = "", interval = "" }) {
     interval === "" ||
     typeof interval !== "string"
   ) {
-    resultCode = 400;
-    resultBody = '{error: "Invalid symbol parameter"}';
-    return { resultCode, resultBody };
+    return {
+      ok: false,
+      code: 400,
+      response: null,
+      error: "Invalid symbol parameter",
+    };
   }
 
   return binanceapiutils("klines", { symbol, interval }, false, false);
