1120be7d98909f5ec7a6739bbf9df6b535124d9f
Dániel Nováki <szept26@gmail.com>
2025-01-17 15:57:14 +0100

    Revert "minor"
    This reverts commit 358006bd49891b2fa7d801a5e678320c7333f7cd.

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -8,7 +8,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: `Method not allowed: ${req.method}` });
+    return res.status(405).json({ error: "Method not allowed" });
   }
 
   const { email, password } = req.body;
